217cabcc33561ab7c0754d6658b5cd3340b378ca
zanadukhan <cchiu231998@hotmail.ca>
2025-02-09 13:09:34 -0800

    implemented basic routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
@@ -6,15 +7,45 @@ import Content from './components/main-page'
 import SignIn from './components/sign-in'
 import SignUp from './components/sign-up'
 
-function App() {
-  return(
-  <>
-  <Header />
-  {/* <Content /> */}
-  {/* <SignIn /> */}
-  <SignUp />
-  <Footer />
-  </>
+const Home = () => {
+  return (
+    <>
+    <Header />
+    <Content />
+    <Footer />
+    </>
+  )
+}
+
+const Register = () => {
+  return (
+    <>
+    <Header />
+    <SignUp />
+    <Footer />
+    </>
+  )
+  }
+
+const Login = () => {
+  return (
+    <>
+    <Header />
+    <SignIn />
+    <Footer />
+    </>
+  )
+}
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
