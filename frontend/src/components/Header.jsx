1f2772a63934588d44b535e71dec80f1835993b0
zanadukhan <cchiu231998@hotmail.ca>
2025-02-09 13:17:48 -0800

    links between pages implemented

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,12 @@
+import { Link } from 'react-router-dom'
+
 const Header = () => {
     return (
         <header className="bg-[#8CDBE0] flex items-center justify-between text-2xl h-12 inset-shadow-md">
-            <a href="#" className='font-[Climate_Crisis] ml-10'>Gigsup</a>
+            <Link to='/' className='font-[Climate_Crisis] ml-10'>Gigsup</Link>
             <ul className="flex gap-4 mr-6">
-                <li><a href="#" class="hover:underline me-4 md:me-6">Login</a></li>
-                <li><a href="#" class="hover:underline me-4 md:me-6">Register</a></li>
+                <li><Link to='/login' class="hover:underline me-4 md:me-6">Login</Link></li>
+                <li><Link to='/register' class="hover:underline me-4 md:me-6">Register</Link></li>
                 <li><a href="#" class="hover:underline me-4 md:me-6">Contact</a></li>
             </ul>
         </header>
