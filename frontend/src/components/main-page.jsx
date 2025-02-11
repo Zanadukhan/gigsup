0d708f1e139b8335f49b89c5f7912ccae7ed03b3
zanadukhan <cchiu231998@hotmail.ca>
2025-02-11 08:57:41 -0800

    minor improvements to main page UI

--- frontend/src/components/main-page.jsx
+++ frontend/src/components/main-page.jsx
@@ -7,15 +7,15 @@ import { Link } from 'react-router-dom';
 const Card = (props) => {
     return (
         <div className='mt-10'>
-            <div className="max-w-sm bg-[#CFFF81] border-[#FFB9B8] border-solid border-8 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+            <div className="mx-5 max-w-sm bg-[#CFFF81] border-[#FFB9B8] border-solid border-8 rounded-lg shadow-sm">
                 <a href="#">
                     <img  src={props.img} alt={props.alt} />
                 </a>
                 <div className="p-5">
                     <a href="#">
-                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{props.header}</h5>
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">{props.header}</h5>
                     </a>
-                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 bg-[#CFFF81]">{props.text}</p>
+                    <p className="mb-3 font-normal text-gray-700 bg-[#CFFF81]">{props.text}</p>
                 </div>
             </div>
 
@@ -30,7 +30,7 @@ const Content = () => {
         <div className='w-2/4'>
          <h1 className="mb-10 text-6xl font-[Climate_Crisis] pt-8 pl-10">Ready to get the juicy goods on your next career path?</h1>
          <p className=" pl-10 w-3/4 text-xl font-normal">Discover your next career move with Gigsup, the AI-powered platform that matches your strengths, interests, and skills with career paths that truly fit you. We provide clear career insights—like roles, skills, compensation, stress levels, and flexibility—so you can save time, avoid unnecessary education costs, and find a job that suits you.</p>
-         <Link to='/register' className=" mt-6 shadow-xl w-48 px-12 outline block ml-11 mt-6 [#8CDBE0] hover:bg-red-300 py-2 px-4 rounded-full">Register Now</Link>
+         <Link to='/register' className="text-center mt-6 shadow-xl w-48 px-12 outline block ml-11 mt-6 [#8CDBE0] hover:bg-red-300 py-2 px-4 rounded-full mb-5">Register Now</Link>
          </div>
          <div className="w-1/3 flex justify-end">
                 <img className="object-none h-full w-100" src={handSqueezeLime} alt="Hand squeezing lime"/>
